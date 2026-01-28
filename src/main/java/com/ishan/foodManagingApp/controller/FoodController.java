81448d15a3b9cae3dd4fdc3949858056a47fd6ce
Ishan Deshpande <deshpandeishan75@gmail.com>
2026-01-28 17:36:07 +0530

    add crud operations

--- src/main/java/com/ishan/foodManagingApp/controller/FoodController.java
+++ src/main/java/com/ishan/foodManagingApp/controller/FoodController.java
@@ -1,13 +1,72 @@
 package com.ishan.foodManagingApp.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.ishan.foodManagingApp.model.Food;
+import com.ishan.foodManagingApp.service.FoodService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class FoodController {
 
 
+    private final FoodService service;
+
+    public FoodController(FoodService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/")
+    public String Greet() {
+        return "Welcome Sir!";
+    }
+
+    @GetMapping("/fooditems")
+    public ResponseEntity<List<Food>> getFoodItems() {
+        return new ResponseEntity<>(service.getFoodItems(),HttpStatus.OK);
+    }
+
+    @PostMapping("/fooditem")
+    public ResponseEntity<?> addFoodItem(@RequestBody Food foodItem) {
+        try {
+            service.addFoodItem(foodItem);
+            return new ResponseEntity<>("Food item added", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/multiplefooditems")
+    public ResponseEntity<?> addFoodItems(@RequestBody List<Food> multipleFoodItems) {
+        try {
+            service.addFoodItems(multipleFoodItems);
+            return new ResponseEntity<>("Food item added", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/fooditem")
+    public ResponseEntity<?> updateFoodItem(@RequestBody Food updatedItem) {
+        try {
+            service.updateFoodItem(updatedItem);
+            return new ResponseEntity<>("Items details updated", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/fooditem")
+    public ResponseEntity<?> deleleItem(@RequestBody Food foodItem) {
+        try {
+            service.deleteFoodItem(foodItem);
+            return new ResponseEntity<>("Food item deleted", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/ishan/foodManagingApp/service/FoodService.java
+++ src/main/java/com/ishan/foodManagingApp/service/FoodService.java
@@ -1,7 +1,41 @@
 package com.ishan.foodManagingApp.service;
 
+import com.ishan.foodManagingApp.model.Food;
+import com.ishan.foodManagingApp.repository.FoodRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodService {
+
+    private final FoodRepo repo;
+    public FoodService(FoodRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<Food> getFoodItems() {
+        return repo.findAll();
+    }
+
+    public void addFoodItem(Food foodItem) {
+        repo.save(foodItem);
+    }
+
+    public void addFoodItems(List<Food> multipleFoodItems) {
+        repo.saveAll(multipleFoodItems);
+    }
+
+    public void updateFoodItem(Food updatedItem) {
+        Food foodItem = repo.findById(updatedItem.getFoodId())
+                .orElseThrow(() -> new RuntimeException("Food item not found!"));
+        foodItem.setFoodName(updatedItem.getFoodName());
+        foodItem.setPrice(updatedItem.getPrice());
+        foodItem.setCategory(updatedItem.getCategory());
+        repo.save(foodItem);
+    }
+
+    public void deleteFoodItem(Food foodItem) {
+        repo.deleteById(foodItem.getFoodId());
+    }
 }
