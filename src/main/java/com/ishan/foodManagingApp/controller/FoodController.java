3732ef84bc3ec6d8a0aa55c7420cf33bdb93021e
Ishan Deshpande <deshpandeishan75@gmail.com>
2026-02-02 14:18:25 +0530

    fix put request handling

--- src/main/java/com/ishan/foodManagingApp/controller/FoodController.java
+++ src/main/java/com/ishan/foodManagingApp/controller/FoodController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("https://foodmanagingapp.netlify.app/")
 @RequestMapping("/api")
 public class FoodController {
 
