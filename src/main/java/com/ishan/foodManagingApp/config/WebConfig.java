c4bec449b0973ca359ce9224297b2cf6a4ae158a
Ishan Deshpande <deshpandeishan75@gmail.com>
2026-02-02 10:39:51 +0530

    add config file

---
+++ src/main/java/com/ishan/foodManagingApp/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.ishan.foodManagingApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://foodmanagingapp.netlify.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
